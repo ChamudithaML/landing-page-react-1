cae331498e1b8d0ca18377c1ead5803bc861bd10
Chamuditha <lilan.2019660@iit.ac.lk>
2025-04-17 21:59:18 +0530

    app tsx changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+import { BrowserRouter } from 'react-router-dom'; // Route, Routes
+import Navbar from './components/Layouts/Navbar';
+import SectionOne from './components/sections/SectionOne';
+import SectionTwo from './components/sections/SectionTwo';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <div>HI</div>
-    </>
+    <BrowserRouter>
+      <Navbar></Navbar>
+      <SectionOne></SectionOne>
+      <SectionTwo></SectionTwo>
+    </BrowserRouter>
   )
 }
 
